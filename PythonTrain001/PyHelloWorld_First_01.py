cc999f097f8f4e6f887f5286970bb9792213315b
Paulo SEC Costa <psc.dev@outlook.pt>
2024-02-24 13:17:30 +0000

    Primeira versao

---
+++ PythonTrain001/PyHelloWorld_First_01.py
@@ -0,0 +1,14 @@
+#Teste Python 2024-02-24
+nome='Paulo'
+GR1,GR2,GR3,GR4,GR5="Diogo Costa","Cláudio Ramos","Samuel Portugal","Francisco Meixedo","Gonçalo Ribeiro"
+TrofLigaPT=TrofTacaPortugal=TrofTacaLiga=TrofSuperTaca="FC Porto"
+GRPT=["Diogo Costa","Rui Patricio","Anthony Lopes","Rui Silva"]
+GR_PT_12,GR_PT_01,GR_PT_22,GR_PT_23=GRPT
+picoiso=float("3.14159265")
+print("Olá! Eu sou o "+nome+" e estou cá para dizer: HELLO WORLD!!!!!")
+if 5 > 2:
+        print("5 é maior que 2!")
+print(picoiso)
+print(type(picoiso))
+print(GR1,GR2,GR3)
+print (GRPT)
